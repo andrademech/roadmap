e036ec4b64672e093fe05052156754d91e3f40cc
Herberth Andrade <andrade.engmecanica@gmail.com>
2022-07-12 21:01:57 -0300

    novas-alterações

--- roadmap/dark-light-theme/index.js
+++ roadmap/dark-light-theme/index.js
@@ -1,5 +1,13 @@
 let dark = document.querySelector("#theme");
 
-dark.addEventListener("change", () => {
+dark.addEventListener("click", () => {
   document.body.classList.toggle("dark");
+
+  console.log(dark.getAttribute('src'));
+  if(dark.getAttribute('src') == dark.getAttribute('src', 'img/light-btn.png')) {
+    dark.setAttribute('src', 'img/dark-btn.png');
+  } else {
+    dark.setAttribute('src', 'img/light-btn.png');
+  }
 });
+ 
